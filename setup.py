197e949270ecd16f20a8d36091b342245101062b
Ed Summers <ehs@pobox.com>
2020-09-03 18:40:44 -0400

    upload markdown to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name = 'bibdesk2zotero',
-    version = '0.0.1',
+    version = '0.0.2',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
     url = 'https://github.com/edsu/bibdesk2zotero',
     py_modules = ['bibdesk2zotero',],
     description = 'convert BibDesk BibTeX files for import into Zotero',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires = ['pybtex'],
     entry_points = {'console_scripts': ['bibdesk2zotero = bibdesk2zotero:main']},
 )
